43a94c42aa11e01d5b5e8c67d6bfdcd3ea0ca8c5
Sveinn Björnsson <kbolinn@gmail.com>
2016-10-12 22:00:22 +0000

    refactor

--- src/main/java/is/ru/StringCalculatorSvenni/Calculator.java
+++ src/main/java/is/ru/StringCalculatorSvenni/Calculator.java
@@ -2,25 +2,18 @@ package is.ru.StringCalculatorSvenni;
 
 public class Calculator{
 
-    public static int add(String text){
+    public static int add(String text) {
         int result = 0;
+        if (text != "") {
 
+            String[] numbers = text.split(",|\\\n");
 
-        if(text.contains(",") || text.contains("\n") ){
-
-            String [] numbers = text.split(",|\\\n");
-
-            for ( int i = 0; i < numbers.length; i++ ){
+            for (int i = 0; i < numbers.length; i++) {
                 int intI = Integer.parseInt(numbers[i]);
-                if(intI <= 1000){
+                if (intI <= 1000) {
                     result += intI;
-
                 }
             }
-            return result;
-
-        }else if(text != ""){
-            result = Integer.parseInt(text);
             return result;
         }
         return result;
